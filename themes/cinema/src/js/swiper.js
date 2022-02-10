88261f908496f0a0a1768490091e118da84f1477
Danador <krut76527@gmail.com>
2022-02-10 20:14:48 +0300

    сделал цикл свайперов с фильмами

--- themes/cinema/src/js/swiper.js
+++ themes/cinema/src/js/swiper.js
@@ -1,3 +1,10 @@
 import Swiper from "swiper";
 
-// var swiper = new Swiper('.swiper-container', {});
+var swiper = new Swiper('.swiper_film', {
+    slidesPerView: 4,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    spaceBetween: 20,
+});
